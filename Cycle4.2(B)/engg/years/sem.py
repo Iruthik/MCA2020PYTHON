86ab35c490cc6b59620c3ff4d657496132aee354
Iruthik <74663157+Iruthik@users.noreply.github.com>
2021-02-26 13:21:52 +0530

    Cycle 4 engg package

---
+++ Cycle4.2(B)/engg/years/sem.py
@@ -0,0 +1,8 @@
+def staff(username,password):
+    if password == "hello":
+        print("Hi"+ username)
+        print("Welcome")
+def student(username,password):
+    if password == "welcome":
+        print("Hi"+username)
+        print("Welcome to your College site")
