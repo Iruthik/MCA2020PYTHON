c2a8bbbc23a154f0b4f7db98a90913bb105781c5
Iruthik <74663157+Iruthik@users.noreply.github.com>
2021-02-26 12:39:36 +0530

    Cycle 4 college package

---
+++ Cycle4.1(A)/college/all_department/cse.py
@@ -0,0 +1,8 @@
+def admin(password,username):
+    if password == "welcomee":
+        print("Hi" + username)
+        print("Welcome to adminportal of cse department ")
+def cabin(password,username):
+    if password == "hello":
+        print("Hi" + username)
+        print("Welcome to cabin side of cse department")
---
+++ Cycle4.1(A)/college/all_department/it.py
@@ -0,0 +1,8 @@
+def admin(password,username):
+    if password == "welcome":
+        print("Hi" + username)
+        print("Welcome to adminportal of it department ")
+def cabin(password,username):
+    if password == "hellow":
+        print("Hi"+username)
+        print("Welcome to cabin side of it department")
